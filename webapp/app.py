f9a0183703380d328363c8864496ab3184b93347
Deshawn Ross <71471435+dross3121@users.noreply.github.com>
2021-01-12 00:34:02 -0500

    Flatfile version

--- webapp/app.py
+++ webapp/app.py
@@ -1,40 +1,59 @@
 import requests 
 from flask import Flask, render_template, jsonify
-
+import csv
 
 app = Flask(__name__)
 
-def githubjobs():
-    page = 1
-    more_pages = True
-    title = 'title'
-    company = 'company'
-    url = 'url'
-    location = 'location'
-    all_data = []
-
-    while more_pages:
-
-        r= requests.get(f'https://jobs.github.com/positions.json?page={page}')
-
-        for HPS in r.json():
-            results = {
-            "Company" : HPS[company],
-            "Title": HPS[title],
-            "Location": HPS[location],
-             "Url": HPS[url]
-
-            }
-            all_data.append(results)
-
-        nextpage= requests.get(f'https://jobs.github.com/positions.json?page={page+1}')
-
-        if len(nextpage.json()) == 0:
-            more_pages = False
+# def githubjobs():  
 
 
-        page+=1 
-    return all_data
+#     page = 1
+#     more_pages = True
+#     title = 'title'
+#     company = 'company'
+#     url = 'url'
+#     location = 'location'
+#     all_data = []
+
+#     while more_pages:
+
+#         r= requests.get(f'https://jobs.github.com/positions.json?page={page}')
+
+#         for HPS in r.json():
+#             results = {
+#             "Company" : HPS[company],
+#             "Title": HPS[title],
+#             "Location": HPS[location],
+#              "Url": HPS[url]
+
+#             }
+#             all_data.append(results)
+
+#         nextpage= requests.get(f'https://jobs.github.com/positions.json?page={page+1}')
+
+#         if len(nextpage.json()) == 0:
+#             more_pages = False
+
+
+#         page+=1 
+#     return all_data
+
+
+def parseCSVFile():
+    ''' 
+    returns data from merged.csv flat file database 
+    '''
+
+
+    data =[]
+
+    with open('merged.csv', newline='') as f:
+        reader = csv.DictReader(f)
+        print('reading file')
+        for row in reader:
+            data.append(row)
+        return data  
+
 
 
 @app.route('/')
@@ -51,9 +70,13 @@ def about():
 
 @app.route('/jobs/')
 def jobs():
-    githubjob= githubjobs()
+    # githubjob= githubjobs()
+    print(parseCSVFile())
+    '''
+    Dispalys data from merged.csv
+    '''
 
-    return render_template('jobs.html', githubjob= githubjob)
+    return render_template('jobs.html', githubjob= parseCSVFile())
 
             
 
